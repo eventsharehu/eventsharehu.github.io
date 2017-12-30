25a02d05351a5a6d67537e2d0f35f42ca53a9521
BuzasLajos <aztamindenitnekivazze@gmail.com>
2017-12-30 11:40:06 +0100

    Update custom-fb.js

--- GoogleMapsApi/src/custom-fb.js
+++ GoogleMapsApi/src/custom-fb.js
@@ -100,8 +100,10 @@
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!'+ '<br/><br/>' + '<img src="https://graph.facebook.com/' + response.id + '/picture">';
+      document.getElementById('profilname').innerHTML =
+        response.name; 
+	    
+	    //'<img src="https://graph.facebook.com/' + response.id + '/picture">';
 	    
 	     
 	    
