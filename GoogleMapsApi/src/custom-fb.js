a9466cc094324ee4e68d3d6d09b6bf3bb60400ba
BuzasLajos <aztamindenitnekivazze@gmail.com>
2017-12-30 11:52:04 +0100

    Update custom-fb.js

--- GoogleMapsApi/src/custom-fb.js
+++ GoogleMapsApi/src/custom-fb.js
@@ -103,7 +103,7 @@
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!'+ '<br/><br/>' + '<img src="https://graph.facebook.com/' + response.id + '/picture">';
 	    
-	    document.getElementById('profilname').innerText =  response.name;
+	    document.getElementById('profilename').innerText =  response.name;
 	     
 	    
     });
